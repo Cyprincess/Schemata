d83042af0f06298f076d275874ba1b0f8e975a77
Chino Chang <chino@cyprin.co>
2024-03-22 20:56:53 +0800

    feat(Core): add Quota feature

---
+++ src/Schemata.Core/Features/SchemataQuotaFeature.cs
@@ -0,0 +1,31 @@
+#if NET8_0_OR_GREATER
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.RateLimiting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Schemata.Core.Features;
+
+public class SchemataQuotaFeature : FeatureBase
+{
+    public override int Priority => 151_000_000;
+
+    public override void ConfigureServices(
+        IServiceCollection  services,
+        Configurators       configurators,
+        IConfiguration      configuration,
+        IWebHostEnvironment environment) {
+        var configure = configurators.Get<RateLimiterOptions>();
+        services.AddRateLimiter(configure);
+    }
+
+    public override void ConfigureApplication(
+        IApplicationBuilder app,
+        IConfiguration      configuration,
+        IWebHostEnvironment environment) {
+        app.UseRateLimiter();
+    }
+}
+
+#endif
