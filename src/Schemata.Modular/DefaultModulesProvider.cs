0055b9d7b26013bc81662f266357d1940564af37
Chino Chang <chino@cyprin.co>
2024-03-25 13:45:25 +0800

    truncate module version

--- src/Schemata.Modular/DefaultModulesProvider.cs
+++ src/Schemata.Modular/DefaultModulesProvider.cs
@@ -33,16 +33,22 @@ public class DefaultModulesProvider : IModulesProvider
                 continue;
             }
 
-            var display     = assembly.GetCustomAttribute<AssemblyTitleAttribute>();
-            var description = assembly.GetCustomAttribute<AssemblyDescriptionAttribute>();
-            var version     = assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>();
+            var display     = assembly.GetCustomAttribute<AssemblyTitleAttribute>()?.Title;
+            var description = assembly.GetCustomAttribute<AssemblyDescriptionAttribute>()?.Description;
 
-            Modules.Add(new ModuleInfo(       //
-                module.Name,                  //
-                assembly, type,               //
-                display?.Title,               //
-                description?.Description,     //
-                version?.InformationalVersion //
+            var version = assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion;
+            if (string.IsNullOrWhiteSpace(version)) {
+                version = assembly.GetCustomAttribute<AssemblyVersionAttribute>()?.Version;
+            }
+
+            version = GetVersion(version);
+
+            Modules.Add(new ModuleInfo( //
+                module.Name,            //
+                assembly, type,         //
+                display,                //
+                description,            //
+                version                 //
             ));
         }
     }
@@ -54,4 +60,24 @@ public class DefaultModulesProvider : IModulesProvider
     }
 
     #endregion
+
+    private static string? GetVersion(string? version) {
+        if (string.IsNullOrWhiteSpace(version)) {
+            return null;
+        }
+
+        var index = version.IndexOf('+');
+        if (index == -1) {
+            return version;
+        }
+
+        var core  = version[..index];
+        var build = version[(index + 1)..];
+
+        if (build.Length > 12) {
+            build = build[..12];
+        }
+
+        return $"{core}+{build}";
+    }
 }
