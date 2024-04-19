ea0f8da73416989c8838eb824ec4bd019bcea942
Chino Chang <chino@cyprin.co>
2024-04-19 17:05:03 +0800

    fix IResourceResponsesAdvice

--- src/Schemata.Resource.Foundation/Advices/IResourceResponsesAdvice.cs
+++ src/Schemata.Resource.Foundation/Advices/IResourceResponsesAdvice.cs
@@ -1,9 +1,9 @@
-using System.Collections.Immutable;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Http;
 using Schemata.Abstractions.Advices;
 using Schemata.Abstractions.Entities;
 
 namespace Schemata.Resource.Foundation.Advices;
 
-public interface IResourceResponsesAdvice<TSummary> : IAdvice<ImmutableArray<TSummary>?, HttpContext>
+public interface IResourceResponsesAdvice<TSummary> : IAdvice<IEnumerable<TSummary>?, HttpContext>
     where TSummary : class, IIdentifier;
