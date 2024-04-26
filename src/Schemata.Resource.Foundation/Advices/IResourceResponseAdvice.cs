a43aa50e50f3ebf983a1a48a6a60df4c81b75c2e
Chino Chang <chino@cyprin.co>
2024-04-26 17:39:05 +0800

    update IResourceResponseAdvice

--- src/Schemata.Resource.Foundation/Advices/IResourceResponseAdvice.cs
+++ src/Schemata.Resource.Foundation/Advices/IResourceResponseAdvice.cs
@@ -4,6 +4,6 @@ using Schemata.Abstractions.Entities;
 
 namespace Schemata.Resource.Foundation.Advices;
 
-public interface IResourceResponseAdvice<TEntity, TDetail> : IAdvice<TEntity, TDetail?, HttpContext>
+public interface IResourceResponseAdvice<TEntity, TDetail> : IAdvice<TEntity?, TDetail?, HttpContext>
     where TEntity : class, IIdentifier
     where TDetail : class, IIdentifier;
